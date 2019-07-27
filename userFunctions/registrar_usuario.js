29d3e6ea658296ad8e60ccea2c459dc0627593c5
merRen22 <renatomercadoluna@hotmail.com>
2019-07-27 14:05:37 -0500

    use registration  with cognito trigger

--- userFunctions/registrar_usuario.js
+++ userFunctions/registrar_usuario.js
@@ -2,9 +2,46 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+const TABLE_USERS = process.env.TABLE_USERS;
+
 exports.registrar_usuario = async function(event, context, callback){
-    String email = event.request.userAttributes.email;
     //CRETEATE USER IN DYNAMO DB
+
+    //Only trigger when user has confirmed his email and singup in cognito
+    //CognitTrigger :: PostAuthentication
+    if(event.eventType === 'SignUp'){
+
+    console.log("Correo de usuario " + event.request.userAttributes.email);
+    var params = {
+        TableName: TABLE_USERS,
+        Item: {
+            'Email' : {S: event.request.userAttributes.email}
+        }
+    };
+
+    /*
+
+            'Payments':{M: [""]},
+            'Trips':{M: [""]},
+    
+    */
+    
+    // Call DynamoDB to add the item to the table
+    console.log("antes de crear usuario");
+    await ddb.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error" + err);
+            //callback(null,{body: JSON.stringify({ message: "No se ha podido crear al usuario" })});
+            //context.done()
+        } else {
+            console.log("Successs ?????");
+        }
+    });
+
+    }
+    
+    callback(null, event);
+
     //END FUNCTION
     //https://docs.aws.amazon.com/es_es/cognito/latest/developerguide/user-pool-lambda-pre-sign-up.html 
 }
