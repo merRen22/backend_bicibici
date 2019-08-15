87ae19c662c0645dc763a7260ec284b8e8b42eaa
merRen22 <renatomercadoluna@hotmail.com>
2019-08-15 08:54:56 -0500

    arrelog funcion biciceltas

--- bicycleFunctions/verificar_movimiento.js
+++ bicycleFunctions/verificar_movimiento.js
@@ -1,6 +1,20 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
-var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+let dynamoDB;
+
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+
+
+if (IS_OFFLINE) {
+  dynamoDB = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  });
+} else {
+  dynamoDB = new AWS.DynamoDB.DocumentClient();
+}
 
 function Bike(IsIntervened,BicycleID,Longuitude,Available,IsMoving,Latitude){  
   this.IsIntervened = IsIntervened;
@@ -16,15 +30,13 @@ exports.verificar_movimiento = async function(event, context, callback){
   
   var params = {
     Key: {
-      "BicycleID": {
-        N: json.BicycleID.toString()
-    }, 
+      BicycleID: json.BicycleID
   }, 
   TableName: "Bicycle"
  };
  
   
- const scan = await ddb.getItem(params).promise();
+ const scan = await dynamoDB.get(params).promise();
 
  if(scan ==  null){
      console.log("fue nulo");
@@ -33,7 +45,7 @@ exports.verificar_movimiento = async function(event, context, callback){
  var bike = new Bike(
           scan.Item.IsIntervened.N,
           scan.Item.BicycleID.N,
-          scan.Item.Longuitude.N,
+          scan.Item.Longitude.N,
           scan.Item.Available.N,
           scan.Item.IsMoving.N,
           scan.Item.Latitude.N
@@ -47,17 +59,23 @@ exports.verificar_movimiento = async function(event, context, callback){
     
     var paramsUpdate = {
       TableName: "Bicycle",
-      Item: {
-        'IsIntervened' : {N: bike.IsIntervened},
-        'BicycleID' : {N: json.BicycleID.toString()},
-        'Longuitude' : {N: json.Longuitude.toString()},
-        'Available' : {N: bike.Available},
-        'IsMoving' : {N: bike.IsMoving},
-        'Latitude' : {N: json.Latitude.toString()}    
+      Key: {
+        BicycleID: json.BicycleID
+      },
+      UpdateExpression: 'SET #attr1 =:newLongitude , #attr2 =:newIsMoving ,  #attr3 =:newLatitude',
+      ExpressionAttributeNames: {
+        '#attr1': 'Longitude',
+        '#attr2': 'IsMoving',
+        '#attr3': 'Latitude'
+      },
+      ExpressionAttributeValues: {
+        ':newLongitude': json.Longitude,
+        ':newLatitude': json.Latitude,
+        ':newIsMoving': 1
       }
     };
     
-    await ddb.putItem(paramsUpdate, function(err, data) {
+    await dynamoDB.update(paramsUpdate, function(err, data) {
       if (err) {
         callback(null,{body: JSON.stringify({ message: "Error al realizar el update 😢 " })});
       } else {
