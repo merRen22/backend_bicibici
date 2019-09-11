926a8ce541ed59ecaae0de16d671a19a9f7b5625
merRen22 <renatomercadoluna@hotmail.com>
2019-09-11 17:37:00 -0500

    avances correcion

--- bicycleFunctions/mensaje_emergencia.js
+++ bicycleFunctions/mensaje_emergencia.js
@@ -1,5 +1,11 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
 var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
+var iotdata = new AWS.IotData({ endpoint: 'azkptoochbd3i-ats.iot.us-east-1.amazonaws.com:8883' });
+
 
 let dynamoDB;
 
@@ -16,7 +22,7 @@ if (IS_OFFLINE) {
   dynamoDB = new AWS.DynamoDB.DocumentClient();
 }
 
-function Bike(IsIntervened,BicycleID,Longuitude,Available,IsMoving,Latitude){  
+function Bike(IsIntervened, BicycleID, Longuitude, Available, IsMoving, Latitude) {
   this.IsIntervened = IsIntervened;
   this.BicycleID = BicycleID;
   this.Longuitude = Longuitude;
@@ -25,9 +31,18 @@ function Bike(IsIntervened,BicycleID,Longuitude,Available,IsMoving,Latitude){
   this.Latitude = Latitude;
 }
 
-exports.mensaje_emergencia = async function(event, context, callback){
-  const json = JSON.parse(JSON.stringify(event));
-  console.log(json)
-  callback(null,{body: JSON.stringify({ message: "user notified" })});
+module.exports.verificar_movimiento = serverless(app);
+
+app.use(bodyParser.json({string: false}));
+
+app.post('/verificar_movimiento', (req, res) => {
+    const json = JSON.parse(JSON.stringify(req.body));
+
+    
+    
+    res.json({
+      status: 'success',
+      breaksStatus: json.uuidBike == '086654f0-cba4-11e9-b0ff-43245eef2175'?1:0
+    });
   
-}
+});
