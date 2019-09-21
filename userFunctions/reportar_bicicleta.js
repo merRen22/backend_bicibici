8b2f13da2657b265a43ad4cef7956e4c7c3332b7
merRen22 <renatomercadoluna@hotmail.com>
2019-09-21 13:12:26 -0500

    avance

--- userFunctions/reportar_bicicleta.js
+++ userFunctions/reportar_bicicleta.js
@@ -8,6 +8,7 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 
 const TABLE_REPORT = process.env.TABLE_REPORT;
+const TABLE_USERS = process.env.TABLE_USERS;
 const IS_OFFLINE = process.env.IS_OFFLINE;
 let dynamoDB;
 
@@ -22,25 +23,27 @@ if(IS_OFFLINE){
 
 app.use(bodyParser.json({string: false}));
 
-app.post('/reportar_bicicleta', (req, res) => {
-    var today = new Date();
-    const json = JSON.parse(JSON.stringify(req.body));
-    var _date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + "|" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    
-    const params = {
-      TableName: TABLE_REPORT,
-      Item: {
-          uuidReport : uuidv1(),
-          date: _date,
-          description: json.description,
-          latitude: json.latitude,
-          state : json.state,
-          longitude: json.longitude,
-          uuidBike: json.uuidBike
-      },
+app.post('/reportar_bicicleta', async (req, res) => {
+  
+  var today = new Date();
+  const json = JSON.parse(JSON.stringify(req.body));
+  var _date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear()  + "|" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  
+  //CREATE BIKE REPORT
+  const params = {
+    TableName: TABLE_REPORT,
+    Item: {
+      uuidReport : uuidv1(),
+      date: _date,
+      description: json.description,
+      latitude: json.latitude,
+      state : 1,
+      longitude: json.longitude,
+      uuidBike: json.uuidBike
+    },
   };
 
-  dynamoDB.put(params,(error,result)=>{
+  await dynamoDB.put(params,(error,result)=>{
     if(error){
       console.log(error);
       res.status(400).json({
@@ -48,12 +51,41 @@ app.post('/reportar_bicicleta', (req, res) => {
       })
     }else{
       const {Items} = result;
-      res.json({
-        sucess: true,
-        message: 'El reporte se registro con exito'
-      });
     }
-  });
+  }).promise();
+
+  //UPDATE USER PUNTUATION
+  const paramsPutUser = {
+    TableName: TABLE_USERS,
+    Key: {
+      uuidUser: json.uuidUser,
+    },
+    UpdateExpression: 'SET #attr =:points',
+    ExpressionAttributeNames: {
+      '#attr': 'experience'
+    },
+    ExpressionAttributeValues: {
+      ':points': 5,
+    }
+  };
+
+  //UPDATE USER POINTS
+  await dynamoDB.update(paramsPutUser, function (error, data) {
+    if (error) {
+      console.log(error);
+      res.json({
+        sucess: false,
+        message: 'Error',
+        data: 'No se pudo actualizar los puntos del usuario'
+      });
+    } else {
+      res.status(200).json({
+        sucess: true,
+        message: 'puntuaje actualizado',
+        data: 'Se creo el reporte exitosamente'
+      })
+    }
+  }).promise();
 });
 
 module.exports.reportar_bicicleta = serverless(app);  
